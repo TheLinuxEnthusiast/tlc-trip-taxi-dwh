e325ed0af765720abb02e81acf69bac62345a95b
darren.foley <darren.foley@ucdconnect.ie>
2021-10-21 20:16:26 +0000

    First working commit

---
+++ UpdateGit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get install -y openssh-client
+
+git config --global user.name "darren.foley"
+git config --global user.email "darren.foley@ucdconnect.ie"
+
+mkdir -p $HOME/.ssh
+
+#ssh-keygen -t rsa
+
+git remote add origin git@github.com:AnalyticsEnthusiast/tlc-trip-taxi-dwh.git
